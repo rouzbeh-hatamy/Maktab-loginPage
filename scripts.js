4b87477a2b9ecdf868824e13bf1f227d99328f33
Rouzbeh Hatamy <roozbeh.hatamy@gmail.com>
2020-04-02 03:56:16 +0430

    add login form

---
+++ scripts.js
@@ -0,0 +1,27 @@
+  window.onload = GenerateCaptcha;
+  /* Function to Generat Captcha */
+  function GenerateCaptcha() {
+    var chr1 = Math.ceil(Math.random() * 10) + '';
+    var chr2 = Math.ceil(Math.random() * 10) + '';
+    var chr3 = Math.ceil(Math.random() * 10) + '';
+
+    var str = new Array(4).join().replace(/(.|$)/g, function () {
+      return ((Math.random() * 36) | 0).toString(36)[Math.random() < .5 ? "toString" : "toUpperCase"]();
+    });
+    var captchaCode = str + chr1 + ' ' + chr2 + ' ' + chr3;
+    document.getElementById("txtCaptcha").value = captchaCode
+  }
+
+  /* Validating Captcha Function */
+  function ValidCaptcha() {
+    var str1 = removeSpaces(document.getElementById('txtCaptcha').value);
+    var str2 = removeSpaces(document.getElementById('txtCompare').value);
+
+    if (str1 == str2) return "متن وارد شده صحیح میباشد";
+    return "متن وارد شده اشتباه است";
+  }
+
+  /* Remove spaces from Captcha Code */
+  function removeSpaces(string) {
+    return string.split(' ').join('');
+  }
